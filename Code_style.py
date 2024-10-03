38a0db2f0e7d82c5e4ccb3f6ab21723683278e80
Vladimir-Bachiev <bachievvv@gmail.com>
2024-10-03 16:49:04 +0300

    Add files via upload

---
+++ Code_style.py
@@ -0,0 +1,13 @@
+from typing import final
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i: int = 0
+
+while int(my_list[i]) >= 0:
+    if my_list[i] == 0:
+        i+=1
+        continue
+    else:
+        print(my_list[i])
+        i += 1
+print('Конец программы')
